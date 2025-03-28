664e07831a34c554eab50d0ad9a6d8878577d56e
andrea1932734 <andreacarovelli@gmail.com>
2025-03-28 10:46:20 +0000

    quasi finito

--- chat-app/backend/app.py
+++ chat-app/backend/app.py
@@ -1,14 +1,24 @@
 from flask import Flask, render_template
 from flask_socketio import SocketIO, send
 from flask_cors import CORS  # Importa CORS
+import threading
 
 app = Flask(__name__)
 socketio = SocketIO(app, cors_allowed_origins="*")
+
+def send_timed_message():
+    socketio.emit('message', 'Messaggio inviato dal server dopo 5 secondi!')
 
 
 @app.route('/')
 def index():
     return render_template('index.html')
+
+@socketio.on('connect')
+def handle_connect():
+    print('Client connesso')
+    timer = threading.Timer(5.0, send_timed_message) # Invia il messaggio dopo 5 secondi
+    timer.start()
 
 @socketio.on('message')
 def handle_message(msg):
